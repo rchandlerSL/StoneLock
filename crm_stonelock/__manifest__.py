224741b14dcd6ac085c0fa453e99fef9bd1aa741
Jimson Ma <jimm@odoo.com>
2024-08-29 09:46:50 -0700

    [UPG][4130090]crm_stonelock module upgrade

--- crm_stonelock/__manifest__.py
+++ crm_stonelock/__manifest__.py
@@ -1,23 +1,16 @@
-# -- coding: utf-8 --
 {
-    'name': "Stonelock Opportunity Aging",
-    'summary': "Stonelock opportunity aging",
-    'description': """
+    "name": "Stonelock Opportunity Aging",
+    "summary": "Stonelock opportunity aging",
+    "description": """
     task id: 2308586
     custom field for opportuunity aging""",
-    'author': 'Odoo',
-    'website': 'https://www.odoo.com/',
-
-    'category': 'Custom Development',
-    'version': '1.0',
-    'license': 'OEEL-1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['crm'],
-
-    # always loaded
-    'data': [
-        'views/inherit_crm_form.xml'
-    ],
-    'application': False,
+    "author": "Odoo Development Services",
+    "maintainer": "Odoo Development Services",
+    "website": "https://www.odoo.com/",
+    "category": "Custom Development",
+    "version": "17.0.1.0",
+    "license": "OPL-1",
+    "depends": ["crm"],
+    "data": ["views/inherit_crm_form.xml"],
+    "application": False,
 }
